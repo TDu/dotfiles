d7048d4344b5b1f56369f0a4ca5a24c9a0fbae5d
Thierry Ducrest <thierry@ducrest.net>
2026-03-26 16:24:57 +0100

    Add acp for copilot in codecompanion

--- nvim/.config/nvim/lua/plugins/codecompanion.lua
+++ nvim/.config/nvim/lua/plugins/codecompanion.lua
@@ -3,12 +3,25 @@ return {
 cmd = { "CodeCompanionChat", "CodeCompanionInline", "CodeCompanionCmd" },
   version = "^18.0.0",
   opts = {
-      prompt_library = {
-      }
+    interactions = {
+      chat = {
+        adapter = "copilot",
+      },
+    },
+    adapters = {
+      acp = {
+        copilot_cli = function()
+          return require("codecompanion.adapters").extend("copilot_cli", {})
+        end,
+      },
+    },
+    prompt_library = {
+    }
   },
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
+    "ttgrules/cc-adapter-copilot-cli-acp",
   },
     keys = {
     { "<leader>ct", ":CodeCompanionChat Toggle<cr>", mode = {"n"}, desc = "Code Companion Chat" },
